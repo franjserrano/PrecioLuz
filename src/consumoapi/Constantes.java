f45fb28b6358e9ef6a90daae343755fc8f62e97f
Francisco Serrano <franjserranosan@gmail.com>
2023-08-07 16:10:15 +0200

    Cambios 0708231610

--- src/consumoapi/Constantes.java
+++ src/consumoapi/Constantes.java
@@ -1,6 +1,19 @@
 package consumoapi;
 
+/**
+ * Constantes necesarias para el funcionamiento de la aplicación
+ */
 public class Constantes {
 	static String[] HORAS = {"00-01","01-02","02-03","03-04","04-05","05-06","06-07","07-08","08-09","09-10","10-11","11-12","12-13","13-14","14-15","15-16","16-17","17-18","18-19","19-20","20-21","21-22","22-23","23-24"};
-
+	
+	static String TOTAL_PRECIOS = "https://api.preciodelaluz.org/v1/prices/all?zone=PCB";
+	
+	//Campos del JSON
+	static String FECHA 	 = "date";
+	static String HORA 		 = "hour";
+	static String ES_BARATO  = "is-cheap";
+	static String BAJO_MEDIA = "is-under-avg";
+	static String MERCADO 	 = "market";
+	static String PRECIO 	 = "price";
+	
 }
--- src/consumoapi/ConsumoAPI.java
+++ src/consumoapi/ConsumoAPI.java
@@ -9,28 +9,40 @@ import java.net.HttpURLConnection;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
+/**
+ * Clase de llamada, utilizaremos una API pública para recuperar los precios de la luz
+ */
 public class ConsumoAPI {
 
+	/**
+	 * Clase main. Desde ella realizaremos tres acciones:
+	 * - Llamar a la API pública
+	 * - Leer los datos
+	 * - Pintar la información formateada
+	 * @param args Parámetros por defecto de la función main. Sin uso en esta aplicación. 
+	 */
 	public static void main(String[] args) {
 		// Solicitar petición
 		// https://api.preciodelaluz.org/v1/prices/all?zone=PCB
 		
+		Map<String,DatosHorarios> preciosHora = new TreeMap<String, DatosHorarios>();
 				
 		try {
-			URL url = new URL("https://api.preciodelaluz.org/v1/prices/all?zone=PCB");
+			//Creamos la URL y la conexión al API
+			URL url = new URL(Constantes.TOTAL_PRECIOS);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			
 			conn.setRequestMethod("GET");
 			conn.connect();
 			
-			Map<String,DatosHorarios> preciosHora = new TreeMap<String, DatosHorarios>();
-			
-			// Comprobar si ha sido correcta
+			/*Confirmamos que se haya conectado correctamente.
+			 * Esto es así si la respuesta recibida por el API es 200.
+			 * En caso de no recibir un 200, puede haber pasado algo, por lo que lanzamos una execpión*/
 			int responseCode = conn.getResponseCode();
 			if(responseCode != 200) {
 				throw new RuntimeException("Error :"+responseCode);
 			}else {
-				//Leer datos
+				//Leemos los datos recibidos por el API
 				StringBuilder informationString = new StringBuilder();
 				Scanner scanner = new Scanner(url.openStream());
 				
@@ -40,24 +52,27 @@ public class ConsumoAPI {
 						
 				scanner.close();
 				
+				/* Parseamos a JSON para poder tratarlos con más facilidad, para ello necesitamos
+				 * importar previamente la librería json-simple*/
 				JSONParser jparser = new JSONParser();
 				JSONObject obj = (JSONObject) jparser.parse(informationString.toString());
 				
+				/* Sabemos que el JSON que devuelve tiene siempre los mismos datos separados por hora,
+				 * recorremos cada hora para recoger la información*/
 				for(String hora:Constantes.HORAS) {
 					JSONObject obt = (JSONObject) obj.get(hora);
-					preciosHora.put(hora, new DatosHorarios(obt.get("date").toString(), 
-															obt.get("hour").toString(), 
-															obt.get("is-cheap").toString(), 
-															obt.get("is-under-avg").toString(), 
-															obt.get("market").toString(), 
-															obt.get("price").toString()));
+					preciosHora.put(hora, new DatosHorarios(obt.get(Constantes.FECHA).toString(), 
+															obt.get(Constantes.HORA).toString(), 
+															obt.get(Constantes.ES_BARATO).toString(), 
+															obt.get(Constantes.BAJO_MEDIA).toString(), 
+															obt.get(Constantes.MERCADO).toString(), 
+															obt.get(Constantes.PRECIO).toString()));
 				}
-				
 				
 				/*************************/
 				//Pintar información
 				
-				//System.out.println(informationString);
+				//Pintamos los datos guardados en el Map (TreeMap, ordenado naturalmente por su nombre de key)
 				Iterator it = preciosHora.keySet().iterator();
 				
 				while(it.hasNext()) {
